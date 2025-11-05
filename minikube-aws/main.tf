fd0ea222a0e9d40d5b8eab6325e3bba598bee944
Harinadha Reddy <Harinadha.Reddy@tmdc.io>
2025-11-05 07:18:35 +0530

    file changes

---
+++ minikube-aws/main.tf
@@ -0,0 +1,22 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "my-minikube-kubernetes"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/id_ed25519.pub"
+  aws_subnet_id = "subnet-06442d65b25eb4cbb"
+  hosted_zone = "dotmain.online"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-dashabord/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-metrics-server/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-nginx-ingress/init.sh"
+  ]
+}
---
+++ minikube-aws/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "aws-terraform-remote-state"
+    key = "minikube"
+    region = "us-east-1"
+    dynamodb_table = "aws-terraform-locking"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
